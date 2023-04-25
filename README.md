c90a477efe5336d8d84193760bde83123972a3a2
Lucas Silva <lsgonsilva@gmail.com>
2023-04-25 15:16:52 -0300

    [FEAT] Criação da paginação de usuários

--- README.md
+++ README.md
@@ -14,6 +14,10 @@ Executar as migrations
 
 ### npx sequelize-cli db:migrate
 
+Executar as seeders
+
+### npx sequelize-cli db:seed:all
+
 Rodar o projeto
 
 ### node app.js
--- controllers/users.js
+++ controllers/users.js
@@ -6,16 +6,63 @@ const db = require('./../db/models');
 
 // Criar a rota listar
 router.get("/users", async (req, res) => {
+    
+    const { page = 1 } = req.query;
+    
+    console.log(page);
+
+    // Limite de registros em cada página
+
+    const limit = 5;
+
+    var lastPage = 1;
+
+    const countUser = await db.Users.count();
+
+    console.log(countUser);
+
+    if(countUser !== 0) {
+        lastPage = Math.ceil(countUser / limit);
+        console.log(lastPage);
+    } else {
+        return res.status(400).json({
+            mensagem: "Error: Nenhum usuário encontrado."
+        });
+    }
+
+    console.log((page * limit) - limit); // 2 * 5 - 5 = 5
+
     const users = await db.Users.findAll({
+
         // Indica quais colunas recuperar
         attributes: ['id', 'name', 'email'],
         // Ordena os registros pela coluna ID de forma decrescente
-        order: [['id', 'DESC']]
+        order: [['id', 'DESC']],
+        
+        offset: Number((page * limit) - limit),
+        limit: limit
     });
     // Se encontrar o registro no banco de dados
     if(users) {
+        // Criar objeto com as informações para paginação
+        var pagination = {
+            //Caminho
+            path: '/users',
+            // Página atual
+            page,
+            // URL da página anterior
+            prev_page_url: page - 1 >= 1 ? page - 1 : false,
+            // URL da próxima página
+            next_page_url: Number(page) + Number(1) > lastPage ? false : Number(page) + Number(1),
+            // Última página
+            lastPage,
+            // Quantidade de registros
+            total: countUser
+        };
+
         return res.json({
-            users
+            users,
+            pagination
         });
     } else {
         return res.status(400).json({
